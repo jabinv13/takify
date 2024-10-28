2159fbbce65d58cfeed6228a9b8fe88b9376806f
jabin-v <jabinv03@gmail.com>
2024-10-28 13:51:01 +0530

    add logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { createAdminClient } from "@/lib/appwrite";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
   console.log(createAdminClient);
   return (
-    <div className="flex gap-4  ">
-      <Button size="lg">Primary</Button>
-      <Button variant="destructive">Primary</Button>
-      <Button variant="teritary">Primary</Button>
+    <div className="flex gap-4">
+      only visible to authorized users
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
