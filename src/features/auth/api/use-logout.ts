b9bbbf0b7f0d57d17512ab47204a9830c5f49410
jabin-v <jabinv03@gmail.com>
2024-10-30 21:52:25 +0530

    fetch workspace in the sidebar

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -20,6 +20,7 @@ export const useLogout = () => {
       // window.location.reload();
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
   });
 
