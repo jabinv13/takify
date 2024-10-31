7aa807a7ccd53d7109fe459436bbb8346ce1159f
jabin-v <jabinv03@gmail.com>
2024-10-31 15:52:22 +0530

    add remove image

--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -144,16 +144,34 @@ export const EditWorkspaceForm = ({
                           disabled={isPending}
                           onChange={handleImageChange}
                         />
-                        <Button
-                          type="button"
-                          disabled={isPending}
-                          variant="teritary"
-                          size="xs"
-                          className="w-fit mt-2"
-                          onClick={() => inputRef.current?.click()}
-                        >
-                          UploadImage
-                        </Button>
+                        {field.value ? (
+                          <Button
+                            type="button"
+                            disabled={isPending}
+                            variant="destructive"
+                            size="xs"
+                            className="w-fit mt-2"
+                            onClick={() => {
+                              field.onChange(null);
+                              if (inputRef.current) {
+                                inputRef.current.value = "";
+                              }
+                            }}
+                          >
+                            Remove image
+                          </Button>
+                        ) : (
+                          <Button
+                            type="button"
+                            disabled={isPending}
+                            variant="teritary"
+                            size="xs"
+                            className="w-fit mt-2"
+                            onClick={() => inputRef.current?.click()}
+                          >
+                            UploadImage
+                          </Button>
+                        )}
                       </div>
                     </div>
                   </div>
