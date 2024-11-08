2bc3424c1d8e53a419e8f43d8d61480cc61181d0
jabin-v <jabinv03@gmail.com>
2024-11-08 20:25:16 +0530

    logo error

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export const Sidebar = () => {
   return (
     <aside className="h-full bg-neutral-100 p-4 w-full">
       <Link href="/">
-        <Image src="logo.svg" alt="Sidebar logo" width={164} height={48} />
+        <Image src="/logo.svg" alt="Sidebar logo" width={164} height={48} />
       </Link>
       <DottedSeparator className="my-4" />
       <WorkspaceSwitcher />
